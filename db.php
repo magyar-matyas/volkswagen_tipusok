f7d61f4fe8666bdfb3f20c95ae0fae6f29488e41
magyar-matyas <magyar.matyas@students.jedlik.eu>
2025-05-26 13:01:54 +0200

    db.php

---
+++ db.php
@@ -0,0 +1,13 @@
+<?php
+$host = 'localhost'; // Változtasd meg, ha nem helyi adatbázist használsz
+$dbname = 'volkswagen_db';
+$username = 'root';
+$password = '';
+
+try {
+    $pdo = new PDO("mysql:host=$host;dbname=$dbname", $username, $password);
+    $pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+} catch (PDOException $e) {
+    echo 'Connection failed: ' . $e->getMessage();
+}
+?>
